8b17f3bf50aec9853e412b9cbc8b884de1f5fb3f
Hangjust <giolutidze1@gmail.com>
2023-11-10 07:46:17 +0000

    yes

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ let xValue = 0,
  yValue = 0;
 
  window.addEventListener("mousemove", (e) => {
-    let speedx = el.dataset.speedx
     xValue = e.clientX - window.innerWidth / 2;
-    yValue =e.clientY - window.innerHeight / 2;
+    yValue = e.clientY - window.innerHeight / 2;
 
 
-    parallax_el.forEach(el => {
-        el.style.transform = `translateX(calc(-100% + ${-xValue * speedx}px)) translateY(calc(-100% + ${yValue}px))`;
-    })
- })
+    parallax_el.forEach((el) => {
+     let speedx = el.dataset.speedx;
+     let speedy = el.dataset.speedy;
+     el.style.transform = `translateX(calc(-50% + ${-xValue * speedx}px)) translateY(calc(-50% + ${yValue * speedy}px))`;
+    });
+});
